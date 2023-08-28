7400081ead2d1885c61c0fbdd246c57fdae81ab9
Jae Hee <travel237corp@naver.com>
2023-08-28 15:33:52 +0900

    오프잭트 객채 필드와 메소드로 이루어짐 클래스는 필드 메소드 생성자로 이루어짐

---
+++ src/basic09/d905instance.java
@@ -0,0 +1,13 @@
+package basic09;
+
+public class d905instance {
+    public static void main(String[] args){
+     // 인스턴스
+     // 오브잭트
+     // 객채  : 객채는 2가지로 구성되어있다   (완벽한 물건자체 ,  속성  [가로크기 , 세로 크기 , 브랜드 ],기능[켜다,끄다다)
+        // 1.속성 (값 ,필드 ,property, attribute)
+        // 2. 기능 (메소드 ,함수 )
+
+
+    }
+}
---
+++ src/basic09Class/d090class.java
@@ -0,0 +1,33 @@
+package basic09Class;
+
+public class d090class {
+    public static void main(String[] args) {
+        //하나에 단에 여러 클래스가
+        MyClass01 m1 =new MyClass01();
+        // new란 연산자와 //  MyClass01(); 생성자를 만든다.
+        //참조값을 알아야한다.
+        System.out.println("System.identityHashCode(m1) =" + System.identityHashCode(m1) );
+
+        System.out.println("m1.age = " + m1.age); //
+        m1.age=30;
+        System.out.println("m1.age = " + m1.age);
+        
+        MyClass01 m2 =m1;
+        
+        System.out.println("m2.age = " + m2.age);
+        
+        m2.age=80;
+        System.out.println("m2.age = " + m2.age);
+    }
+}
+class MyClass01{
+    //instance fiedld
+    //인스턴스가 가져야하는 속성들  이 나열되어야 한다. class라는 설꼐도에
+    int age ;
+    
+    
+    //instance methods
+    //인스턴스가 가져하는 기능들 (메소드)
+    
+
+}
